0c84d14e9b4c10478c7dfcdee1001454b7004761
Vijay Soni (vs4vijay) <vs4vijay@gmail.com>
2020-03-31 11:38:53 +0530

    Brew formula update for vega version v0.0.1

--- Formula/vega.rb
+++ Formula/vega.rb
@@ -2,16 +2,16 @@
 class Vega < Formula
   desc "vega"
   homepage ""
-  version "0.0.0"
+  version "0.0.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/srijanone/vega/releases/download/v0.0.0/vega_0.0.0_mac_x86_64.tar.gz"
-    sha256 "c16f9caa709c28e3e6ea2ea97b88f494afd297a84f0f86ac52593f7dbb71e594"
+    url "https://github.com/srijanone/vega/releases/download/v0.0.1/vega_0.0.1_mac_x86_64.tar.gz"
+    sha256 "21f950a3282784f6d0ba3810186a6884bba6557d67bb9b2318b083029684150a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/srijanone/vega/releases/download/v0.0.0/vega_0.0.0_linux_x86_64.tar.gz"
-      sha256 "7d98c914a06af8f229218117f5b9d89db818bfb1b07dadf056daa5d571a6c601"
+      url "https://github.com/srijanone/vega/releases/download/v0.0.1/vega_0.0.1_linux_x86_64.tar.gz"
+      sha256 "8575454eb7640ad076c0f704835a44b4034e15c82c175dd67c272dee55935b4e"
     end
   end
 
